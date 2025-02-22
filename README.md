a608c53cf2774a5087666f6c0717977bbf4fcb46
inesvar <90340779+inesvar@users.noreply.github.com>
2025-02-22 13:14:11 +0100

    Displaying even more various rectangles

--- README.md
+++ README.md
@@ -8,8 +8,9 @@ EG_SIMULATOR_DUMP=screenshots/screenshot.png cargo +nightly run
 - cas ou la bordure est plus épaisse que le rectangle : OK
 - propreté du code pour les coins : OK
 
-- calculer le nombre de cercles à dessiner en fonction de `border_size` :
-- les positioner de façon symétrique au centre :
+- calculer le nombre de cercles à dessiner en fonction de `border_size` : OK
+- les positioner de façon symétrique au centre : ACK (inutile)
+- rendre le code plus propre (notamment pour le round !) : 
 
 - extraire le code des coins dans une fonction à part du trait `ExtendedStyledDrawable`
 et expliquer le principe
--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,23 @@ use embedded_graphics::{
 use embedded_graphics_simulator::{OutputSettings, SimulatorDisplay, Window};
 
 const PADDING: i32 = 16;
+const RECTANGLE_SIZES: [Size; 3] = [Size::new(10, 64), Size::new(42, 42), Size::new(48, 24)];
+const COLORS: [Rgb888; 3] = [
+    Rgb888::CSS_ORANGE_RED,
+    Rgb888::CSS_CRIMSON,
+    Rgb888::CSS_AQUAMARINE,
+];
 
 /// Draws all embedded-graphics primitives.
 fn draw_primitives<D>(target: &mut D, w: u32) -> Result<(), D::Error>
 where
     D: DrawTarget<Color = Rgb888>,
 {
-    let rectangle_size = Size::new(10, 64);
-    let rectangle_top_left = Point::new(64, 64) - rectangle_size;
-    let colors = [
-        Rgb888::CSS_ORANGE_RED,
-        Rgb888::CSS_CRIMSON,
-        Rgb888::CSS_AQUAMARINE,
-    ];
-
     for i in 0..3 {
-        Rectangle::new(rectangle_top_left, rectangle_size)
-            .translate(Point::new((64 + PADDING) * i, 0))
-            .into_styled(PrimitiveStyle::with_stroke(
-                colors[i as usize],
-                w + i as u32,
-            ))
+        let rectangle_top_left = Point::new(64, 64) - RECTANGLE_SIZES[i];
+        Rectangle::new(rectangle_top_left, RECTANGLE_SIZES[i])
+            .translate(Point::new(0, (64 + PADDING) * i as i32))
+            .into_styled(PrimitiveStyle::with_stroke(COLORS[i], w))
             .draw(target)?;
     }
 
@@ -48,16 +44,23 @@ fn main() -> Result<(), core::convert::Infallible> {
     // Instead of directly drawing to the display a `TranslatedDrawTarget` is created by
     // using `display.translated(position)`. This translates all drawing operations in `draw_shapes`
     // by 10 pixels in the x and y direction.
-    let mut position = Point::new(10, 10);
+    let mut position = Point::new(10, 16);
+    draw_primitives(&mut display.translated(position), 1)?;
+
+    position.x += 64 + PADDING;
+    draw_primitives(&mut display.translated(position), 2)?;
+
+    position.x += 64 + PADDING;
+    draw_primitives(&mut display.translated(position), 3)?;
+
+    position.x += 64 + PADDING;
     draw_primitives(&mut display.translated(position), 4)?;
 
-    // Draw the primitives using a medium stroke.
-    position.y += 64 + PADDING;
-    draw_primitives(&mut display.translated(position), 10)?;
+    position.x += 64 + PADDING;
+    draw_primitives(&mut display.translated(position), 8)?;
 
-    // Draw the primitives using a thick stroke.
-    position.y += 64 + PADDING;
-    draw_primitives(&mut display.translated(position), 25)?;
+    position.x += 64 + PADDING;
+    draw_primitives(&mut display.translated(position), 16)?;
 
     Window::new("Strokes", &OutputSettings::default()).show_static(&display);
 
