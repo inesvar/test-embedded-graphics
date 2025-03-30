caa26fecf0961675c28844b4ea220fa7e669dd44
inesvar <90340779+inesvar@users.noreply.github.com>
2025-03-30 17:00:41 +0200

    Draw naive dotted lines manually
    First issues noticed:
    - the rounding in this example and in the library are different
    - on thin 45° lines, due to the distance between dots being too small,
    dots are not always separated by gaps
    - almost horizontal / vertical dotted lines look wonky
    - on 45° lines, dots can look too big or too small
    
    The last 3 problems are especially noticeable on thin lines =>
    why not make a specific code for thin lines like for the rectangle?
    This code could keep half the points in the iterator instead of trying
    to draw small dots which look like rectangles anyway.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
-//! # Example: Primitive stroke styles
-//!
-//! This example demonstrates the different stroke styles available for primitives.
+// inspired by: https://github.com/embedded-graphics/examples
 
-#[allow(unused_imports)]
+#![no_std]
+extern crate embedded_graphics;
+extern crate embedded_graphics_simulator;
+
+#[allow(unused)]
+use core::cmp::max;
 use embedded_graphics::{
+    geometry::{real::real_impl::Real, Point, Size},
     pixelcolor::Rgb888,
     prelude::*,
-    primitives::{
-        Circle, CornerRadii, Ellipse, Line, PrimitiveStyle, PrimitiveStyleBuilder, Rectangle,
-        RoundedRectangle, StrokeAlignment, StrokeStyle, Triangle,
-    },
+    primitives::{Circle, Line, PrimitiveStyle},
 };
 use embedded_graphics_simulator::{OutputSettings, SimulatorDisplay, Window};
 
 const PADDING: i32 = 16;
-const RECTANGLE_SIZES: [Size; 3] = [Size::new(10, 64), Size::new(42, 42), Size::new(48, 24)];
+const SIZES: [Size; 3] = [Size::new(10, 64), Size::new(42, 42), Size::new(48, 24)];
 const COLORS: [Rgb888; 3] = [
     Rgb888::new(0xF8, 0xF4, 0xE3),
     Rgb888::new(0x58, 0xA4, 0xB0),
     Rgb888::new(0x0C, 0x7C, 0x59),
 ];
+
+fn dot_positions_with_dotted_corners(size: Size, dot_size: u32) -> impl Iterator<Item = Size> {
+    let length = (size.width * size.width + size.height * size.height).isqrt();
+    let nb_dots = length / (2 * dot_size);
+    // let nb_dots_x = size.width / (2 * dot_size);
+    // let nb_dots_y = size.height / (2 * dot_size);
+    // let nb_dots = max(nb_dots_x, nb_dots_y);
+    let dot_offset_x = Real::from(size.width) / Real::from(nb_dots);
+    let dot_offset_y = Real::from(size.height) / Real::from(nb_dots);
+
+    let idx_iter = 0..=nb_dots;
+    idx_iter.map(move |idx| {
+        Size::new(
+            (dot_offset_x * Real::from(idx)).round().into(),
+            (dot_offset_y * Real::from(idx)).round().into(),
+        )
+    })
+}
 
 /// Draws all embedded-graphics primitives.
 fn draw_primitives<D>(target: &mut D, w: u32) -> Result<(), D::Error>
@@ -27,23 +46,42 @@ where
     D: DrawTarget<Color = Rgb888>,
 {
     for i in 0..3 {
-        let rectangle_top_left = Point::new(64, 64) - RECTANGLE_SIZES[i];
-        let fill_style = PrimitiveStyleBuilder::from(&PrimitiveStyle::with_stroke(COLORS[i], w))
-            .stroke_alignment(StrokeAlignment::Center)
-            .build();
-        let dot_style = PrimitiveStyleBuilder::from(&fill_style)
-            .stroke_color(COLORS[(i + 1) % 3])
-            .stroke_style(Some(StrokeStyle::Dotted))
-            .build();
+        let mut rectangle_top_left = Point::new(64, 64) - SIZES[i];
+        let line_style = PrimitiveStyle::with_stroke(COLORS[i], w);
+        let fill_style = PrimitiveStyle::with_fill(COLORS[(i + 1) % 3]);
 
-        Rectangle::new(rectangle_top_left, RECTANGLE_SIZES[i])
+        /* Rectangle::with_center(rectangle_top_left, Size::new_equal(w))
+        .translate(Point::new(0, (64 + PADDING) * i as i32))
+        .into_styled(fill_style)
+        .draw(target)?; */
+
+        Line::new(rectangle_top_left, rectangle_top_left + SIZES[i])
             .translate(Point::new(0, (64 + PADDING) * i as i32))
-            .into_styled(fill_style)
+            .into_styled(line_style)
             .draw(target)?;
-        Rectangle::new(rectangle_top_left, RECTANGLE_SIZES[i])
+
+        rectangle_top_left += Size::new(4 * w, 0);
+
+        Line::new(rectangle_top_left, rectangle_top_left + SIZES[i])
             .translate(Point::new(0, (64 + PADDING) * i as i32))
-            .into_styled(dot_style)
+            .into_styled(line_style)
             .draw(target)?;
+
+        for translation in dot_positions_with_dotted_corners(SIZES[i], w) {
+            Circle::with_center(rectangle_top_left + translation, w)
+                .translate(Point::new(0, (64 + PADDING) * i as i32))
+                .into_styled(fill_style)
+                .draw(target)?;
+        }
+
+        rectangle_top_left += Size::new(4 * w, 0);
+
+        for translation in dot_positions_with_dotted_corners(SIZES[i], w) {
+            Circle::with_center(rectangle_top_left + translation, w)
+                .translate(Point::new(0, (64 + PADDING) * i as i32))
+                .into_styled(fill_style)
+                .draw(target)?;
+        }
     }
 
     Ok(())
@@ -52,11 +90,6 @@ where
 fn main() -> Result<(), core::convert::Infallible> {
     let mut display = SimulatorDisplay::<Rgb888>::new(Size::new(512, 256));
 
-    // Draw the primitives using a thin stroke.
-    //
-    // Instead of directly drawing to the display a `TranslatedDrawTarget` is created by
-    // using `display.translated(position)`. This translates all drawing operations in `draw_shapes`
-    // by 10 pixels in the x and y direction.
     let mut position = Point::new(10, 16);
     draw_primitives(&mut display.translated(position), 1)?;
 
